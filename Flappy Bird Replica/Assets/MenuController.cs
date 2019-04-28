3bae23f769d1f7738c6e58f371a9defa9b5efdda
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-04-28 20:47:43 +0300

    Basic Menu

---
+++ Flappy Bird Replica/Assets/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject menuButton;
+    public GameObject muteSoundButton;
+    public GameObject okButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MenuButton()
+    {
+        playButton.SetActive(false);
+        menuButton.SetActive(false);
+        muteSoundButton.SetActive(true);
+        okButton.SetActive(true);
+    }
+
+    public void OkButton()
+    {
+        playButton.SetActive(true);
+        menuButton.SetActive(true);
+        muteSoundButton.SetActive(false);
+        okButton.SetActive(false);
+    }
+
+    public void MuteSoundButton()
+    {
+
+    }
+}
